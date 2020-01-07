fbae51d0acd59c49c7d0950cdaa7f402a4331989
valentinapalma <valentinapalma@hotmail.se>
2020-01-07 09:57:34 +0100

    importing HomeScreen

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import HomeScreen from './Screens/HomeScreen.js'
 
 export default class App extends Component {
   render() {
