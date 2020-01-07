c1f51f4765900cd8bbad3b5220b14a074ad96eb6
martinkassar <40896885+MartinKassar@users.noreply.github.com>
2020-01-07 11:59:09 +0100

    saving

--- Frontend/Components/TaskManagerComponent.js
+++ Frontend/Components/TaskManagerComponent.js
@@ -1,69 +1,110 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import styles from '../Style';
+
+// import DateTimePicker from '@react-native-community/datetimepicker';
 
 
 export default class TaskManagerComponent extends Component {
-	
-	constructor(props) {
-		super(props);
-		
-		this.state = {
-			title: '',
-			description: '',
-			date: undefined,
-			error: ''
-		};
-	}
-	
-	getTaskById = (taskId, e) => {
-		fetch("http://localhost:3000/todos/" + taskId)
-		.then((response) => {
-			return response.json();
-		})
-		.then((data) => {
-			this.setState({
-				title: data.title,
-				description: data.description,
-				date: data.date
-			});
-		})
-		.catch((error) => {
-			this.setState({
-				error: error
-			})
-		})
-	}
-	
-  render() {
-	return (
-	  <View style={styles.container}>
-		<Text style={styles.message}>Add your task!</Text>
-		<TextInput placeholder = 'Title' style={styles.form}/>
-		<TextInput placeholder = 'Description' style={styles.form}/>
-		<TextInput placeholder = 'Date' style={styles.form}/>
-		<Button title='ADD' style={styles.button}/>
-	  </View>
-	);
-  }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: '',
+            description: '',
+            date: new Date('2020-06-12T14:42:42'),
+            error: ''
+            
+        };
+    }
+
+    getTaskById = (taskId, e) => {
+        fetch("http://localhost:3000/todos/" + taskId)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({
+                    title: data.title,
+                    description: data.description,
+                    date: data.date
+                });
+            })
+            .catch((error) => {
+                this.setState({
+                    error: error
+                })
+            })
+    }
+
+    handleTitle = event => {
+        this.setState({
+            title: event.target.value
+        })
+
+    }
+
+    handleDescription = event => {
+        this.setState({
+            description: event.target.value
+        })
+    }
+
+
+
+    postTask = () => {
+         fetch("http://localhost:3000/todos", {
+       method: 'POST',
+       body: JSON.stringify({
+           title: this.state.title,
+           description: this.state.description,
+           date: this.state.date
+       }),
+       headers: {
+         'Content-Type': 'application/json'
+       }
+     }).then(res => res.json())
+       .then(response => console.log('Success:', JSON.stringify(response)))
+       .catch(error => console.error('Error:', error));
+    }
+
+    dateChange = event => {
+        this.setState({
+            date: event.target.value
+        })
+
+    }
+
+    render() {
+        console.log(this.state.title)
+        console.log(this.state.date)
+        console.log(this.state.description)
+        return (
+            <View style={styles.container}>
+                <Text style={styles.message}>Add your task!</Text>
+                <TextInput placeholder='Title'
+                    style={styles.form}
+                    value={this.state.title}
+                    onChange={this.handleTitle} />
+                <TextInput placeholder='Description'
+                    style={styles.form}
+                    value={this.state.description}
+                    onChange={this.handleDescription} />
+                <TextInput placeholder='Date'
+                    style={styles.form}
+                    value={this.state.date}
+                    onChange={this.dateChange} />
+                <Button title='ADD'
+                    style={styles.button}
+                    onPress={this.postTask} />
+                    {/* <DateTimePicker value={this.state.date}
+                    display='calendar'/>         */}
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-    message: {
-        fontSize: 20,
-        textAlign: 'center'
-    },
-  container: {
-    padding: 50
-  },
-  form: {
-    width: '80%',
-    padding: 50,
-    borderBottomColor: 'black',
-     borderBottomWidth: 1,
-     textAlign: 'center'
-  },
-  button: {
-      textAlign: 'center',
-      fontSize: 40
-  }
+const style = StyleSheet.create({
+   
 });
