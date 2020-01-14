13fa0add082e5a0bab3bb464a912b82275e3efdb
valentinapalma <valentinapalma@hotmail.se>
2020-01-14 14:22:50 +0100

    Some styling fixes for the iOS

--- Frontend/Style.js
+++ Frontend/Style.js
@@ -8,70 +8,74 @@ export default StyleSheet.create({
     marginTop: 10,
   },
   logoView: {
-      borderBottomWidth: 1,
-      borderColor: 'lightgray',
-      width: '100%',
-      justifyContent: 'center',
-      alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'lightgray',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   infoContainer: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 20,
-      height: '100%'
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    //height: '100%'
   },
   infoText: {
-      fontSize: 20,
-      lineHeight: 25
+    fontSize: 20,
+    lineHeight: 25
+  },
+  addButton: {
+    fontSize: 20,
+    marginTop: 20
   },
   flatList: {
     marginTop: 10,
-    width: '100%'
+    width: '100%',
+    height: '80%',
   },
   eachTask: {
+    height: 120,
     margin: 10,
     padding: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 6.22,
-    elevation: 3,
+    //shadowColor: "#000",
+    //shadowOffset: {
+      //width: 0,
+      //height: 2,
+    //},
+    //shadowOpacity: 0.22,
+    //shadowRadius: 6.22,
+    //elevation: 3,
   },
   iconView: {
-    position: "absolute",
-    top: "20px",
-    right: "20px",
-    display: "block"
+    //position: "absolute",
+    //top: 20,
+    //right: 20,
   },
   iconDelete: {
-    marginLeft: "10px",
-    fontSize: "20px"
+    marginLeft: 10,
   },
   iconEdit: {
-    fontSize: "20px"
+    //fontSize: "20px"
   },
   iconRemoveMessage: {
 	  margin: 15
   },
   iconCheck: {
 	  position: "absolute",
-	  top: "5px",
-	  left: "100px",
-	  fontSize: "20px"
+	  top: 5,
+	  left: 100,
+	  //fontSize: "20px"
   },
   iconTimes: {
 	  position: "absolute",
-	  top: "5px",
-	  left: "160px",
-	  fontSize: "20px"
+	  top: 5,
+	  left: 160,
+	  //fontSize: "20px"
   },
   iconArrowLeft: {
-	  position: "absolute",
-	  top: "-50px",
-	  left: "-60px",
+	  //position: "absolute",
+	  //top: "-50px",
+	  //left: "-60px",
   },
   
   //TaskManager
@@ -80,11 +84,12 @@ export default StyleSheet.create({
     textAlign: 'center'
   },
   container: {
-      padding: 50
+      //padding: 50
+      width: '100%'
   },
   form: {
-      width: '80%',
-      padding: 50,
+      width: '100%',
+      padding: 20,
       borderBottomColor: 'black',
       borderBottomWidth: 1,
       textAlign: 'center'
